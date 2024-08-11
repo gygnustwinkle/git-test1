313de76a0382d61ef11a479ba5a5ba727087ad78
xjs <1536452867@qq.com>
2024-08-11 17:07:31 +0800

    first commit

---
+++ src/main/java/com/yql/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.yql.git;
+
+/**
+ * @Author yql
+ * @Date 2024/8/11 17:01
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
